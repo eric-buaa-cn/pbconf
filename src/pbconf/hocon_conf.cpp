50259f3dc0da4072128cc9ce74cebcf8fdb52a00
eric <eric.buaa.cn@gmail.com>
2018-06-28 15:23:39 +0800

    hocon support float and float list

--- src/pbconf/hocon_conf.cpp
+++ src/pbconf/hocon_conf.cpp
@@ -431,6 +431,79 @@ inline bool OnNodeFor<bool>(
 }
 // End bool
 
+// Begin float
+template <>
+inline bool get<float>(shared_value node, float& value) {
+    if (!node || node->value_type() != ::hocon::config_value::type::NUMBER) {
+        return false;
+    }
+    string literal = node->transform_to_string();
+
+    try {
+        value = boost::lexical_cast<float>(literal);
+    } catch (boost::bad_lexical_cast& e) {
+        return false;
+    }
+    return true;
+}
+
+template <>
+inline bool OnNodeForSingle<float>(
+        shared_value node,
+        const FieldDescriptor* field,
+        Message& parent_msg,
+        string& err_msg) {
+    float value{0.};
+    if (!get(node, value)) {
+        butil::StringAppendF(&err_msg, "Expect float value at:%s",
+                field->full_name().c_str());
+        return false;
+    }
+    const Reflection* reflection = parent_msg.GetReflection();
+    reflection->SetFloat(&parent_msg, field, value);
+    return true;
+}
+
+template <>
+inline bool OnNodeForRepeated<float>(
+        shared_value node,
+        const FieldDescriptor* field,
+        Message& parent_msg,
+        string& err_msg) {
+    if (!node || node->value_type() != ::hocon::config_value::type::LIST) {
+        butil::StringAppendF(&err_msg, "Wrong type");
+        return false;
+    }
+
+    auto real_node = std::static_pointer_cast<const ::hocon::config_list>(node);
+    const Reflection* reflection = parent_msg.GetReflection();
+
+    float value{0.};
+    for (auto citr = real_node->begin(); citr != real_node->end(); ++citr) {
+        if (!get(*citr, value)) {
+            return false;
+        }
+        reflection->AddFloat(&parent_msg, field, value);
+    }
+    return true;
+}
+
+template <>
+inline bool OnNodeFor<float>(
+        shared_value node,
+        const FieldDescriptor* field,
+        Message& parent_msg,
+        string& err_msg) {
+    const Reflection* reflection = parent_msg.GetReflection();
+
+    if (field->is_repeated()) {
+        return OnNodeForRepeated<float>(node, field, parent_msg, err_msg);
+    } else {
+        return OnNodeForSingle<float>(node, field, parent_msg, err_msg);
+    }
+}
+// End float
+
 static bool OnNode(
         shared_value node,
         const FieldDescriptor* field,
@@ -503,6 +576,9 @@ static bool OnNode(
     }
     if (field->cpp_type() == FieldDescriptor::CPPTYPE_BOOL) {
         return OnNodeFor<bool>(node, field, parent_msg, err_msg);
+    }
+    if (field->cpp_type() == FieldDescriptor::CPPTYPE_FLOAT) {
+        return OnNodeFor<float>(node, field, parent_msg, err_msg);
     }
     return true;
 }

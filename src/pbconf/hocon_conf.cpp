47b8f873084c87bd044022c7523ef4eda6396245
eric <eric.buaa.cn@gmail.com>
2018-06-28 14:38:05 +0800

    hocon support int64 and int64 list

--- src/pbconf/hocon_conf.cpp
+++ src/pbconf/hocon_conf.cpp
@@ -139,6 +139,77 @@ inline bool OnNodeFor<int32_t>(
 }
 // End int32_t
 
+// Begin int64_t
+template <>
+inline bool get<int64_t>(shared_value node, int64_t& value) {
+    if (!node || node->value_type() != ::hocon::config_value::type::NUMBER) {
+        return false;
+    }
+    string literal = node->transform_to_string();
+
+    try {
+        value = boost::lexical_cast<int64_t>(literal);
+    } catch (boost::bad_lexical_cast& e) {
+        return false;
+    }
+    return true;
+}
+
+template <>
+inline bool OnNodeForSingle<int64_t>(
+        shared_value node,
+        const FieldDescriptor* field,
+        Message& parent_msg,
+        string& err_msg) {
+    int64_t value{0};
+    if (!get(node, value)) {
+        return false;
+    }
+    const Reflection* reflection = parent_msg.GetReflection();
+    reflection->SetInt64(&parent_msg, field, value);
+    return true;
+}
+
+template <>
+inline bool OnNodeForRepeated<int64_t>(
+        shared_value node,
+        const FieldDescriptor* field,
+        Message& parent_msg,
+        string& err_msg) {
+    if (!node || node->value_type() != ::hocon::config_value::type::LIST) {
+        butil::StringAppendF(&err_msg, "Wrong type");
+        return false;
+    }
+
+    auto real_node = std::static_pointer_cast<const ::hocon::config_list>(node);
+    const Reflection* reflection = parent_msg.GetReflection();
+
+    int64_t value{0};
+    for (auto citr = real_node->begin(); citr != real_node->end(); ++citr) {
+        if (!get(*citr, value)) {
+            return false;
+        }
+        reflection->AddInt64(&parent_msg, field, value);
+    }
+    return true;
+}
+
+template <>
+inline bool OnNodeFor<int64_t>(
+        shared_value node,
+        const FieldDescriptor* field,
+        Message& parent_msg,
+        string& err_msg) {
+    const Reflection* reflection = parent_msg.GetReflection();
+
+    if (field->is_repeated()) {
+        return OnNodeForRepeated<int64_t>(node, field, parent_msg, err_msg);
+    } else {
+        return OnNodeForSingle<int64_t>(node, field, parent_msg, err_msg);
+    }
+}
+// End int64_t
+
 static bool OnNode(
         shared_value node,
         const FieldDescriptor* field,
@@ -199,6 +270,9 @@ static bool OnNode(
     }
     if (field->cpp_type() == FieldDescriptor::CPPTYPE_INT32) {
         return OnNodeFor<int32_t>(node, field, parent_msg, err_msg);
+    }
+    if (field->cpp_type() == FieldDescriptor::CPPTYPE_INT64) {
+        return OnNodeFor<int64_t>(node, field, parent_msg, err_msg);
     }
     return true;
 }
